7958961441100350eaad80a254f598615309b5f8
Rhullian Damião <rhullian4545@gmail.com>
2022-03-01 17:45:06 -0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Rhullian Damião on 01/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
